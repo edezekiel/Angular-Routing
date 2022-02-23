1ae3036049edaeafcdc2cb7bcc38742732a12176
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-02-23 09:23:01 -0600

    Add ProductResolver to products.module routes

--- APM-Start/src/app/products/product.module.ts
+++ APM-Start/src/app/products/product.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { SharedModule } from '../shared/shared.module';
+import { ProductResolver } from './product-resolver.service';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
-
-import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     SharedModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', component: ProductDetailComponent },
-      { path: 'products/:id/edit', component: ProductEditComponent },
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent,
+        resolve: { productResult: ProductResolver },
+      },
+      {
+        path: 'products/:id/edit',
+        component: ProductEditComponent,
+        resolve: { productResult: ProductResolver },
+      },
     ]),
   ],
   declarations: [
