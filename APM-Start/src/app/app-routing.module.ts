edfaaacae1eb766c66df95731e170cfb5b0508a5
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-02-24 15:42:01 -0600

    Lazy load ProductsModule, create custom preloading strategy

--- APM-Start/src/app/app-routing.module.ts
+++ APM-Start/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { routes } from './app.routes';
+import { PreloadSelectedModulesList } from './preload-selected.strategy.service';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadSelectedModulesList }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
